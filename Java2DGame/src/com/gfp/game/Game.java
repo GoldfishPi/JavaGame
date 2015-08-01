3556cd97043495501214bdc824782782b2928fea
GoldfishPi <erikbadger777@gmail.com>
2015-08-01 17:26:21 -0500

    Added a comment

--- Java2DGame/src/com/gfp/game/Game.java
+++ Java2DGame/src/com/gfp/game/Game.java
@@ -20,7 +20,7 @@ import com.gfp.game.level.Tiles.Tile;
 
 public class Game extends Canvas implements Runnable
 {
-//
+//This is a game i am working on.
 	private static final long serialVersionUID = 1L;
 
 	public static final int WIDTH = 160;
