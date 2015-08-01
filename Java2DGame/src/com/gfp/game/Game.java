9ffa11174cbc85d39c8c2a646af00e3cb8ed811c
GoldfishPi <erikbadger777@gmail.com>
2015-08-01 17:19:58 -0500

    First change(yay)

--- Java2DGame/src/com/gfp/game/Game.java
+++ Java2DGame/src/com/gfp/game/Game.java
@@ -20,7 +20,7 @@ import com.gfp.game.level.Tiles.Tile;
 
 public class Game extends Canvas implements Runnable
 {
-
+//
 	private static final long serialVersionUID = 1L;
 
 	public static final int WIDTH = 160;
